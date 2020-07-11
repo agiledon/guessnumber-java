3320419f256783721a5dcb6fd3db1f1bea29d150
yi <brucezhang@tom.com>
2020-07-11 11:50:23 +0800

    refactor to validRange() method

--- src/main/java/xyz/zhangyi/tdd/guessnumber/Answer.java
+++ src/main/java/xyz/zhangyi/tdd/guessnumber/Answer.java
@@ -4,16 +4,18 @@ import xyz.zhangyi.tdd.guessnumber.exception.InvalidAnswerException;
 
 public class Answer {
     public Answer(int number1, int number2, int number3, int number4) {
+        validRange(number1, number2, number3, number4);
+    }
+
+    private void validRange(int number1, int number2, int number3, int number4) {
+        validRange(number1);
+        validRange(number2);
+        validRange(number3);
+        validRange(number4);
+    }
+
+    private void validRange(int number1) {
         if (number1 < 0 || number1 > 9) {
-            throw new InvalidAnswerException("out of range (0, 9)");
-        }
-        if (number2 < 0 || number2 > 9) {
-            throw new InvalidAnswerException("out of range (0, 9)");
-        }
-        if (number3 < 0 || number3 > 9) {
-            throw new InvalidAnswerException("out of range (0, 9)");
-        }
-        if (number4 < 0 || number4 > 9) {
             throw new InvalidAnswerException("out of range (0, 9)");
         }
     }
