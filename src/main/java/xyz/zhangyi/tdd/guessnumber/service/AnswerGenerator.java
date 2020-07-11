db469d963277d9ba671fd66e5e7c3d1a7c6fbd9a
yi <brucezhang@tom.com>
2020-07-11 14:38:44 +0800

    refactor generate() method

--- src/main/java/xyz/zhangyi/tdd/guessnumber/service/AnswerGenerator.java
+++ src/main/java/xyz/zhangyi/tdd/guessnumber/service/AnswerGenerator.java
@@ -12,16 +12,28 @@ public class AnswerGenerator {
     }
 
     public Answer generate() {
+        ArrayList<Integer> numbers = generateUniqueCorrectNumbers();
+        return new Answer(numbers.get(0), numbers.get(1), numbers.get(2), numbers.get(3));
+    }
+
+    private ArrayList<Integer> generateUniqueCorrectNumbers() {
         ArrayList<Integer> numbers = new ArrayList<>(4);
 
         for (int i = 0; i < 4; i++) {
             int number = random.next();
-            while (number < 0 || number > 9 || numbers.contains(number)) {
+            while (isOutOfRange(number) || isDuplicate(numbers, number)) {
                 number = random.next();
             }
             numbers.add(number);
         }
+        return numbers;
+    }
 
-        return new Answer(numbers.get(0), numbers.get(1), numbers.get(2), numbers.get(3));
+    private boolean isDuplicate(ArrayList<Integer> numbers, int number) {
+        return numbers.contains(number);
+    }
+
+    private boolean isOutOfRange(int number) {
+        return number < 0 || number > 9;
     }
 }
